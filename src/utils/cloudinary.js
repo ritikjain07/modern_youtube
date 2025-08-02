7188b6f9747d37ecb1187f043e94ea117acc94ce
ritikjain07 <ritikjain4560@gmail.com>
2025-08-02 14:57:05 +0530

    feat: add multer middleware for file uploads and cloudinary configuration for image storage

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,40 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+
+
+// Configuration
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET
+    });
+
+
+
+const uploadOnCloudinary = async(localFilePath) => {
+    try{
+        if(!localFilePath) return null
+        cloudinary.uploader.upload(localFilePath), {
+            resource_type: "auto"
+        }
+        console.log("file is uploded")
+    } catch (error){
+        fs.unlinkSync(localFilePath)
+        return null;
+    }
+}
+
+export {uploadOnCloudinary}
+
+ // Upload an image
+     const uploadResult = await cloudinary.uploader
+       .upload(
+           'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
+               public_id: 'shoes',
+           }
+       )
+       .catch((error) => {
+           console.log(error);
+       });
+    
+    console.log(uploadResult);
